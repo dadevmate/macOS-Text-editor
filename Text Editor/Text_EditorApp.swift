d1bfb8a1aff28efa9fc231e9db5448565dc007f4
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-03-04 16:19:30 +0800

    Initial Commit

---
+++ Text Editor/Text_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_EditorApp.swift
+//  Text Editor
+//
+//  Created by Nethan on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Text_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
